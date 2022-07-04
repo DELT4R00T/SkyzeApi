4842be77b6eb8ac577ff04177b8fdd95d744f736
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-07-04 15:14:31 +0700

    Update games.js

--- server/games.js
+++ server/games.js
@@ -2,9 +2,40 @@ const express = require('express')
 var router = express.Router();
 
 const { tebakgambar } = require('../scraper/index') 
+const { asahotak, tebakkata, caklontong } = require('@bochilteam/scraper')
+
 
 router.get('/tebakgambar', async(req, res) => {
 	var hasil = await tebakgambar()
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+router.get('/asahotak', async(req, res) => {
+	var hasil = await asahotak()
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+router.get('/tebakkata', async(req, res) => {
+	var hasil = await tebakkata()
+	try {
+		res.json(hasil)
+	} catch(err) {
+		console.log(err)
+		res.json({ message: 'Ups, error' })
+	}
+})
+
+router.get('/caklontong', async(req, res) => {
+	var hasil = await caklontong()
 	try {
 		res.json(hasil)
 	} catch(err) {
